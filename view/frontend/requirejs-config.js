bd5e60f93af856acf5c44eaf2ca24dd4cdbac7d5
Maurits Meester <maurits@e-mmer.nl>
2021-01-29 15:05:22 +0100

    :sparkles: (basic) Load + easy to use tooltip component

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,13 @@
+var config = {
+    map: {
+        '*': {
+            'VTooltip': 'Deadsimple_VTooltipLibrary/js/lib/v-tooltip.min',
+        }
+    },
+    shim: {
+        'Vuelidate': ['Vue']
+    },
+    paths: {
+        'DeadsimpleTooltipComponents': 'Deadsimple_VTooltipLibrary/js/components'
+    }
+};
---
+++ view/frontend/web/js/components/tooltip.vue
@@ -0,0 +1,34 @@
+<template>
+    <span v-if="showTooltip" v-tooltip="{content, offset}"><slot /></span>
+</template>
+
+<script>
+define([
+    'Vue',
+    'VTooltip'
+], function (Vue, VTooltip) {
+
+    Vue.component('tooltip', {
+        template: template,
+        directives: {
+            tooltip: VTooltip.VTooltip,
+        },
+        props: {
+            content: {
+                type: String,
+                default: ''
+            },
+            offset: {
+                type: Number,
+                default: 0
+            }
+        },
+        computed: {
+            showTooltip() {
+                return this.content !== ''
+            }
+        }
+    });
+
+});
+</script>
